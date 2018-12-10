5eae4737aa22bdc5ad67cc3f2106c0d00ebf71f4
Ashutosh Singh <ashutosh.singh@smava.de>
2018-12-10 12:23:25 +0100

    added deploy steps

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# CI Build
 docker build -t vinp:0.1 .
 docker tag vinp:0.1 a4sh3u/vinp:latest
 docker push a4sh3u/vinp:latest
+
+
+# Deploy on local machine
+docker-compose up -d
